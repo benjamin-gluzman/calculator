5980def46a17966b67f2c73083dd197432305078
Benjamin Gluzman <benjamin.gluzman@gmail.com>
2025-12-19 19:16:35 -0500

    Add most of calculator functionality

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
-let num1, num2, operator;
+let num1 = "", num2 = "", operator = "";
+let calcPerformed = false;
+
+const calculator = document.querySelector(".calculator");
+const display = document.querySelector(".display");
+display.value = "0";
+
+const NUMS = "0123456789.";
+const OPERATORS = "/*-+"
+
+calculator.addEventListener("click", (event) => {
+    let button = event.target.id;
+    if(NUMS.includes(button)) {
+        if(operator === "") {
+            if(!calcPerformed)
+                num1 += button;
+            else
+                num1 = button;
+            calcPerformed = false;
+            display.value = num1;
+        }
+        else {
+            num2 += button;
+            display.value = num2;
+        }
+    }
+    else if(OPERATORS.includes(button) ) {
+        operator = button;
+    }
+    else if(button === "=" && operator != "" && num2 != "") {
+        num1 = operate(+num1, +num2, operator);
+        num2 = "";
+        operator = "";
+        display.value = num1;
+        calcPerformed = true;
+    }
+    else if(button === "clear") {
+        num1 = "";
+        num2 = "";
+        operator = "";
+        calcPerformed = false;
+        display.value = "0";
+    }
+});
 
 
 function operate(num1, num2, operator) {
@@ -7,6 +50,7 @@ function operate(num1, num2, operator) {
     if(operator === "*") return multiply(num1, num2);
     return divide(num1, num2);
 }
+
 function add(a, b) {
     return a + b;
 }
